f5f8f6718ef65b5be12c836e1709c53b88035429
toanvu <toanvu2311@gmail.com>
2023-07-21 09:54:59 +0700

    Project EduSys

---
+++ Project/src/com/edusys/entity/HocVien.java
@@ -0,0 +1,40 @@
+package com.edusys.entity;
+
+public class HocVien {
+    private int maHV;
+    private int maKH;
+    private String maNH;
+    private double diem;
+
+    public int getMaHV() {
+        return maHV;
+    }
+
+    public void setMaHV(int maHV) {
+        this.maHV = maHV;
+    }
+
+    public int getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(int maKH) {
+        this.maKH = maKH;
+    }
+
+    public String getMaNH() {
+        return maNH;
+    }
+
+    public void setMaNH(String maNH) {
+        this.maNH = maNH;
+    }
+
+    public double getDiem() {
+        return diem;
+    }
+
+    public void setDiem(double diem) {
+        this.diem = diem;
+    }
+}
---
+++ Project/src/com/edusys/entity/NhanVien.java
@@ -0,0 +1,45 @@
+package com.edusys.entity;
+
+public class NhanVien {
+    private String maNV;
+    private String matKhau;
+    private String hoTen;
+    private boolean vaiTro = false;
+
+    @Override
+    public String toString() {
+        return this.hoTen;
+    }
+
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public boolean getVaiTro() {
+        return vaiTro;
+    }
+
+    public void setVaiTro(boolean vaiTro) {
+        this.vaiTro = vaiTro;
+    }
+}
